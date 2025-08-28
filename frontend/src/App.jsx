0882c5f8374f96e1011ad172e8dacba3618bdbe2
Satyam Singh <satyamrajput7239@gmail.com>
2025-08-28 19:56:34 +0530

    feat: Add pages files and add routing to them

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,23 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import SideBar from "./components/SideBar";
+import ChatBox from "./components/ChatBox";
+import Credits from "./pages/Credits";
+import Community from "./pages/Community";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <SideBar />
+        <Routes>
+          <Route path="/" element={<ChatBox />} />
+          <Route path="/credits" element={<Credits />} />
+          <Route path="/community" element={<Community />} />
+        </Routes>
+      </div>
+    </>
+  );
 }
 
 export default App;
