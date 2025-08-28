54e1a95eda158f8f3417ddb186ea9b9433fdd30c
Satyam Singh <satyamrajput7239@gmail.com>
2025-08-28 22:18:06 +0530

    feat : add context

---
+++ frontend/src/context/AppContext.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react-refresh/only-export-components */
+import { createContext, useContext } from "react";
+
+const AppContext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+  const value = {};
+
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export const useAppContext = () => useContext(AppContext);
