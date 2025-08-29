d4235ee7b5b3493b443020d108f87760f8554bfa
Satyam Singh <satyamrajput7239@gmail.com>
2025-08-29 18:47:05 +0530

    feat: add state management in AppContext

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,10 +1,58 @@
 /* eslint-disable react-refresh/only-export-components */
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { dummyChats, dummyUserData } from "../assets/assets";
 
 const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
-  const value = {};
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [chats, setChats] = useState([]);
+  const [selectedChats, setSelectedChats] = useState(null);
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+  const fetchUser = async () => {
+    setUser(dummyUserData);
+  };
+
+  const fetchUserChats = async () => {
+    setChats(dummyChats);
+    setSelectedChats([0]);
+  };
+
+  useEffect(() => {
+    if (user) {
+      fetchUserChats();
+    } else {
+      setChats([]);
+      setSelectedChats(null);
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  const value = {
+    navigate,
+    user,
+    setUser,
+    fetchUser,
+    chats,
+    setChats,
+    selectedChats,
+    setSelectedChats,
+    theme,
+    setTheme,
+  };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
