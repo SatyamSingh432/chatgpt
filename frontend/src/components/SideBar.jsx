8f9cb396efd7c2adf26131250fcaa10fd3d389a4
Satyam Singh <satyamrajput7239@gmail.com>
2025-08-31 10:41:54 +0530

    feat: Add view for recents chats in SideBar

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -16,12 +16,10 @@ const SideBar = () => {
         alt=""
         className="w-full max-w-48"
       />
-
       {/* new chat button */}
       <button className="flex justify-center items-center w-full py-2 mt-10 bg-gradient-to-r from-[#A456F7] to-[#3D81F6] text-sm rounded-md cursor-pointer">
         <span className="mr-2 text-xl">+</span>New Chat
       </button>
-
       {/* search conversations */}
       <div className="flex items-center gap-2 p-3 mt-4 border border-gray-400 dark:border-white/20 rounded-md">
         <img src={assets.search_icon} className="w-4 not-dark:invert" alt="" />
@@ -34,6 +32,41 @@ const SideBar = () => {
           placeholder="Search conversations"
           className="text-xs placeholder:text-gray-400 outline-none"
         />
+      </div>
+      {/* recent chats
+       */}
+      {chats.length > 0 && <p className="mt-4 text-sm">Recent Chats</p>}
+      <div className="flex-1 overFlow-y-scroll mt-3 text-sm space-y-3">
+        {chats
+          .filter((chat) =>
+            chat.messages[0]
+              ? chat.messages[0].content
+                  .toLowerCase()
+                  .includes(search.toLowerCase())
+              : chat.name.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((chat) => (
+            <div
+              key={chat._id}
+              className="p-2 px-4 dark:bg-[#57317C]/10 border border-gray-300 dark:border-[#80609F]/15 rounded-md cursor-pointer flex justify-between group"
+            >
+              <div>
+                <p className="truncate w-full">
+                  {chat.messages.length > 0
+                    ? chat.messages[0].content.slice(0, 32)
+                    : chat.name}
+                </p>
+                <p className="text-xs text-gray-500 dark:text-[#B1A6C0]">
+                  {chat.updatedAt}
+                </p>
+              </div>
+              <img
+                src={assets.bin_icon}
+                className="hidden group-hover:block w-4 cursor-pointer not-dark:invert"
+                alt=""
+              />
+            </div>
+          ))}
       </div>
     </div>
   );
